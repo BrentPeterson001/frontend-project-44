254fd57116b1491a8b2d33a88d87c20dd0d311dc
pasta <pc2@gmail.com>
2026-04-18 21:41:16 +0300

    added: brain-calc

---
+++ src/calc.js
@@ -0,0 +1,30 @@
+import { runGame } from './index.js';
+
+const getRandomNumber = (min = 0, max = 100) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+const calculate = (num1, num2, operator) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      throw new Error(`Unknown operator: ${operator}`);
+  }
+};
+  const generateRound = () => {
+  const num1 = getRandomNumber(1, 20);
+  const num2 = getRandomNumber(1, 20);
+  const operators = ['+', '-', '*'];
+  const operator = operators[Math.floor(Math.random() * operators.length)];
+  const question = `${num1} ${operator} ${num2}`;
+  const correctAnswer = String(calculate(num1, num2, operator));
+  return { question, correctAnswer };
+};
+export const runCalcGame = () => {
+  const gameDescription = 'What is the result of the expression?';
+  runGame(gameDescription, generateRound);
+};
--- src/even.js
+++ src/even.js
@@ -1,29 +1,12 @@
-import readlineSync from 'readline-sync';
-
-import { greeting } from './cli.js';
-
+import { runGame } from './index.js';
 const isEven = (number) => number % 2 === 0;
-
+const generateRound = () => {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  const question = String(randomNumber);
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+  return { question, correctAnswer };
+};
 export const runEvenGame = () => {
-  const name = greeting();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const questions = [
-    Math.floor(Math.random() * 100) + 1,
-    Math.floor(Math.random() * 100) + 1,
-    Math.floor(Math.random() * 100) + 1,
-  ];
-  for (let i = 0; i < questions.length; i += 1) {
-    const randomNumber = questions[i];
-    const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
-    console.log(`Question: ${randomNumber}`);
-    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${name}!`);
+  const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
+  runGame(gameDescription, generateRound);
 };
