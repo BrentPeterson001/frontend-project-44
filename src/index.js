ba5f79fdaa372df9fb495b4e3009cbe6153dd9c0
pasta <pc2@gmail.com>
2026-04-20 01:00:18 +0300

    eslint fix

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ export const runGame = (gameDescription, generateRound) => {
     if (userAnswer === correctAnswer) {
       console.log('Correct!')
       correctAnswers += 1
-    } else {
+    }
+    else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
       console.log(`Let's try again, ${name}!`)
       return
